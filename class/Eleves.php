c8bb78dc2e60189747393cd0da8875d3fb5cd3b3
TCHIBOZO_Fr <f.tchibozo@lprs.fr>
2022-09-19 11:25:43 +0200

    add : formulaire de connexion et inscription

--- class/Eleves.php
+++ class/Eleves.php
@@ -25,4 +25,134 @@ class Eleves{
         }
     }
 
+    public function EleveConnexion (){
+
+    }
+
+    public function EleveInscription (){
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdEleves()
+    {
+        return $this->id_eleves;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param mixed $adresse
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValider()
+    {
+        return $this->valider;
+    }
+
+    /**
+     * @param mixed $valider
+     */
+    public function setValider($valider)
+    {
+        $this->valider = $valider;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDomaineEtude()
+    {
+        return $this->domaine_etude;
+    }
+
+    /**
+     * @param mixed $domaine_etude
+     */
+    public function setDomaineEtude($domaine_etude)
+    {
+        $this->domaine_etude = $domaine_etude;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNiveauEtude()
+    {
+        return $this->niveau_etude;
+    }
+
+    /**
+     * @param mixed $niveau_etude
+     */
+    public function setNiveauEtude($niveau_etude)
+    {
+        $this->niveau_etude = $niveau_etude;
+    }
+
+
+
 }

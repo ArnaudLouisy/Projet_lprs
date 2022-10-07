e68ffadc77b03a36596112193729672f1e57b413
TCHIBOZO françois <totofrancois12@gmail.com>
2022-10-07 02:50:57 +0200

    add : creation de la class email

---
+++ class/Email.php
@@ -0,0 +1,21 @@
+<?php
+
+class Email{
+    private $destinataire;
+    private $titre;
+    private $expediteur;
+
+    public function __construct(array $donnees){
+        $this->hydrate($donnees);
+    }
+
+    private function hydrate(array $donnees){
+        foreach ($donnees as $key => $value){
+            $method = 'set'.ucfirst($key);
+
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+}

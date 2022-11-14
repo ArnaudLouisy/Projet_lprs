adf115d4d1f6bb3a2370530e5cc9759672d19dec
LOUISY_Ar <louisy.arnaud@gmail.com>
2022-11-14 12:06:58 +0100

    Add:Integration front end page index.php

--- form/dist/inscription.php
+++ form/dist/inscription.php
@@ -30,7 +30,7 @@ session_start()
             elseif(isset($_SESSION['erreurmotdepasse'])):
                 echo ("<span class='form__span' style='color: red '>".$_SESSION['erreurmotdepasse']."</span>");
             else:
-                echo ("<span class='form__span'>ou adresse email pour créer un compte</span>");
+              //  echo ("<span class='form__span'>ou adresse email pour créer un compte</span>");
             endif;
             ?>
             <input class="form__input" name="nom" type="text" placeholder="Nom">
@@ -70,15 +70,15 @@ session_start()
         <div class="switch__circle"></div>
         <div class="switch__circle switch__circle--t"></div>
         <div class="switch__container" id="switch-c1">
-            <h2 class="switch__title title">Welcome Back !</h2>
-            <p class="switch__description description">To keep connected with us please login with your personal info</p>
-            <button class="switch__button button switch-btn">Se connecter</button>
+            <h1 class="switch__title title">S'inscrire en tant entreprise </h1>
+            <p class="switch__description description">Si vous etes une entreprise cliquez sur le bouton pour vous inscrire </p>
+            <button class="switch__button button switch-btn">s'inscrire eleves </button>
             <form><button formaction="../../index.php" class="switch__button button switch-btn">Accueil</button></form>
         </div>
         <div class="switch__container is-hidden" id="switch-c2">
-            <h2 class="switch__title title">Hello Friend !</h2>
-            <p class="switch__description description">Enter your personal details and start journey with us</p>
-            <button class="switch__button button switch-btn">S'inscrire</button>
+            <h2 class="switch__title title">S'inscrire en tant qu'elèves </h2>
+            <p class="switch__description description">Si vous etes un eleves cliquez sur le bouton pour vous inscrire</p>
+            <button class="switch__button button switch-btn">S'inscrire eleves</button>
         </div>
     </div>
 </div>

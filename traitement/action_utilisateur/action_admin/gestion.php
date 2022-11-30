54cb86ed7200ac5b132a849fa3f76e4e0c5bccb6
François <totofrancois12@gmail.com>
2022-12-01 00:01:36 +0100

    fix : route

--- traitement/action_utilisateur/action_admin/gestion.php
+++ traitement/action_utilisateur/action_admin/gestion.php
@@ -1,24 +1,25 @@
 <?php
-require_once '../../class/Bdd.php';
+require_once '../../../class/Bdd.php';
 $bdd = new Bdd();
 if ($_POST['action']){
     $res=explode("_",$_POST['action']);
     if ($res[1]=="utilisateur"){
-        require_once '../../class/Utilisateur.php';
+        require_once '../../../class/Utilisateur.php';
         $utilisateur = new Utilisateur(array(
             'idutilisateur'=>$res[0]
         ));
         $utilisateur->valider($bdd);
+        header('Location: ');
     }elseif ($res[1]=='offre'){
-        require_once '../../class/Offre.php';
+        require_once '../../../class/Offre.php';
         $offre = new Offre(array(
             'idoffre'=>$res[0]
         ));
         $offre->valider($bdd);
     }elseif ($res[1]=='evenement'){
-        require_once '../../class/Evenement.php';
+        require_once '../../../class/Evenement.php';
         $evenement = new Evenement(array(
-            'idevent'=>$res[0]
+            'idevent'=>$res[0],
         ));
         $evenement->valider($bdd);
     }
